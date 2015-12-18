e0b65dfd32881f590223db23801867d10f52e189
Graham McGregor <dev@grahammcgregor.ca>
2015-12-18 01:18:28 -0500

    Clean up init script, add more comments
    In case some else wants to use this init script, try to make it easier
    to understand and use.

--- README.md
+++ README.md
@@ -1,28 +1,28 @@
 # My Automated Config Setup
 
-I wanted an automated way to quickly setup a shell with my own customizations.  Setup is at ~4
+I wanted an automated way to quickly setup a shell with my own customizations.  Setup is at ~3
 commands right now.
 
 Setup steps:
 
 ```sh
-git clone https://github.com/Graham42/dot_files.git
-cd dot_files
-./init_all.sh
+git clone https://github.com/Graham42/dot_files.git ~/dot_files
+~/dot_files/init_all.sh
 source ~/.bashrc
 ```
 
 # Structure
 
-- All dot files are escaped in this repo with a leading `_` so it's not as easy to forget about
-  files that are hidden. Symbolic links are created with the leading `_`'s removed. Example:
-  `_.vimrc` becomes `.vimrc`
-- Anything in `home/` is linked to `~` and then the directory structure inside is followed
+- Anything in `home/` is linked to the user's home directory and then the directory structure
+  inside is followed
+- All dot files in `home/` are escaped with a leading underscore so it's not as easy to forget
+  about files that are hidden. Symbolic links are created with the leading underscore's removed.
+  Example: `_.vimrc` becomes `.vimrc`
 - Running `init_all.sh` will create and update a marked section in `~/.bashrc`.
-  - `include_common.sh` is the main block content.
-  - If there are files in the `os_specific/` folder, they will be included if the OS check passes.
-    OS specific files require a 2 line header, where the second line is a commented shell command
-    that will return a 0 exit status if it detects the correct OS.
+  - `bashrc_body.sh` is the main block content.
+  - If there are files in the `os_specific_bashrc/` folder, they will be included if the OS check
+    passes.  OS specific files require a 2 line header, where the second line is a commented shell
+    command that will return a zero exit status if it detects the correct OS.
 
     Example:
 
@@ -30,4 +30,4 @@ source ~/.bashrc
     # OS check:
     # cat /etc/*-release | grep '^NAME=\"Arch Linux\"' -q
     ```
-- Any scripts in `setup_scripts` will be run, but they must be executable or the init will fail.
+- Any scripts in `setup_scripts` will be run, but they must be executable or the setup will fail.
--- init_all.sh
+++ init_all.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 # This script is to speed up setting up a new machine with my personal config
 
+THIS_SCRIPT="init_all.sh"
+
+################################################################################
+## Util functions for this script
+
 # Colors for nicer output
 Color_Off="\033[0m"         # Text Reset
 Red="\033[0;31m"            # Red
 Green="\033[0;32m"          # Green
 Yellow="\033[0;33m"         # Yellow
+Cyan="\033[0;36m"           # Cyan
 
 function ls_dirs {
-    ls_type d $1
+    ls_type d $@
 }
 function ls_files {
-    ls_type f $1
+    ls_type f $@
 }
 function ls_type {
     if [ "${2}x" == "x" ]; then
@@ -30,47 +36,70 @@ function loginfo {
 function logwarn {
     echo -e "${Yellow}$@$Color_Off"
 }
-function logerror {
+function logfatal {
     echo -e "${Red}$@$Color_Off"
     exit 1
 }
 
+################################################################################
+## Setup
+
+# This script should be the master, should be run, not sourced
+if [ $THIS_SCRIPT != $(basename $0) ]; then
+    logfatal "This script should be run, not sourced"
+fi
+
+# allow this script to be run from anywhere
+cd $(dirname $0)
+
+loginfo "Updating config for user: $USER"
+
+################################################################################
+## Create/update a block in ~/.bashrc with our modifications
+
+BASH_RC_BODY_FILE="bashrc_body.sh"
+BASH_RC_OS_DIR="os_specific_bashrc"
 
 # WARNING: if header/footer are changed, will mess up auto-update
-bashrc_header="####################_BEGIN_DOT_FILES_AUTOBLOCK_######################"
-bashrc_footer="#####################_END_DOT_FILES_AUTOBLOCK_#######################"
-if [ ! -e include_common.sh ]; then
-    logerror "Missing file 'include_common.sh'"
+BASHRC_HEADER="####################_BEGIN_DOT_FILES_AUTOBLOCK_######################"
+BASHRC_FOOTER="#####################_END_DOT_FILES_AUTOBLOCK_#######################"
+
+if [ ! -e $BASH_RC_BODY_FILE ]; then
+    logfatal "Missing file '$BASH_RC_BODY_FILE'"
 fi
-bashrc_body=$( cat include_common.sh )
-# os specific things
-if [ -d os_specific ]; then
-    for os_file in $( ls_files os_specific ); do
-        os_file_path="os_specific/$os_file"
-        # header of each file determines if it this is the OS it's looking for
-        if eval $( head -n2 $os_file_path | tail -n1  | sed "s/^#\+//g" ) ; then
-            bashrc_body="$bashrc_body\n$( tail -n+3 "os_specific/$os_file" )"
+bashrc_body=$( cat $BASH_RC_BODY_FILE )
+# os specific additions
+if [ -d "$BASH_RC_OS_DIR" ]; then
+    for os_file in $( ls_files "$BASH_RC_OS_DIR" ); do
+        # header of each file determines if this is the OS it's looking for
+        if eval $( head -n2 "$BASH_RC_OS_DIR/$os_file" | tail -n1  | sed "s/^#\+//g" ) ; then
+            bashrc_body="$bashrc_body\n$( tail -n+3 "$BASH_RC_OS_DIR/$os_file" )"
         fi
     done
 fi
-# auto update the include block in ~/.bashrc
-if $(grep "$bashrc_header" ~/.bashrc -q); then
-    echo "Already initialized. Updating..."
+echo "Updating block in ~/.bashrc"
+# auto update the block in ~/.bashrc
+if $(grep "$BASHRC_HEADER" ~/.bashrc -q); then
     escaped_body=$(echo "$bashrc_body" | sed -e':a' -e'N' -e'$!ba' -e's/\n/\\n/g')
-    sed -i'' -e"/^$bashrc_header$/,/^$bashrc_footer$/c $bashrc_header\n$escaped_body\n$bashrc_footer" ~/.bashrc
+    sed -i'' -e"/^$BASHRC_HEADER$/,/^$BASHRC_FOOTER$/c $BASHRC_HEADER\n$escaped_body\n$BASHRC_FOOTER" ~/.bashrc
 else
-    echo "Initializing..."
-    echo -e "\n$bashrc_header\n$bashrc_body\n$bashrc_footer\n" >> ~/.bashrc
+    echo -e "\n$BASHRC_HEADER\n$bashrc_body\n$BASHRC_FOOTER\n" >> ~/.bashrc
 fi
 
-# create symbolic links to users home directory
+################################################################################
+## Create symbolic links to home directory
+#
+# Every thing in the $DOT_FILES/home directory is linked to user's home
+# directory. Underscores are used for to escape dotfiles so they're not hidden
+# in the $DOT_FILES directory.
+
 USER_HOME=$( echo ~ )
 CWD=$(pwd)
-dirs_todo=( "home" )
+dirs_to_link=( "home" )
 i=0
 # loop because bash recursion is ... not great
-while [ "$i" -lt "${#dirs_todo[*]}" ]; do
-    base_dir=${dirs_todo[$i]}
+while [ "$i" -lt "${#dirs_to_link[*]}" ]; do
+    base_dir=${dirs_to_link[$i]}
 
     for _file in $( ls_files $base_dir ); do
         # remove leading '_'s
@@ -88,19 +117,22 @@ while [ "$i" -lt "${#dirs_todo[*]}" ]; do
         ln -s $CWD/$base_dir/$_file $dest_file
     done
 
-    # push any sub dirs onto the todo array
+    # push any sub dirs onto the array of things left to do
     for _dir in $( ls_dirs $base_dir ); do
-        dirs_todo+=( "$base_dir/$_dir" )
+        dirs_to_link+=( "$base_dir/$_dir" )
     done
 
     ((i=i+1))
 done
 
+################################################################################
+## Run any other setup scripts
+
 if [ -d setup_scripts ]; then
-    echo "Running extra setup scripts..."
+    echo "Running extra setup scripts"
     for initscript in $( ls_files setup_scripts ); do
         initscript_path="setup_scripts/$initscript"
-        ./$initscript_path || logerror "Failed to run setup script: $initscript"
+        ./$initscript_path || logfatal "Failed to run setup script: $initscript"
     done
 fi
 
