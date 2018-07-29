6480e957a17868ceb3bb02c42551afd08e7e684c
Graham McGregor <graham@gmcgregor.ca>
2018-07-29 15:16:26 -0400

    Check if run from within tmux

--- setup_scripts/tmux.sh
+++ setup_scripts/tmux.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env bash
+set -xe
+
 TMUX_PLUGIN_HOME=~/.tmux/plugins/tpm
 git clone https://github.com/Graham42/tpm.git ${TMUX_PLUGIN_HOME} 2>/dev/null || \
     (cd ${TMUX_PLUGIN_HOME} && git pull)
 
-tmux source-file ~/.tmux.conf
+[[ ! -z "$TMUX" ]] && tmux source-file ~/.tmux.conf
 
 ${TMUX_PLUGIN_HOME}/bin/install_plugins
 ${TMUX_PLUGIN_HOME}/bin/update_plugins all
 
-if $(uname -s | grep -q Linux) ; then
+if uname -s | grep -q Linux ; then
     if ! which xclip > /dev/null ; then
         echo "You may need to install xclip for tmux to be able to copy to the system clipboard"
     fi
 fi
 
-tmux source-file ~/.tmux.conf
+[[ ! -z "$TMUX" ]] && tmux source-file ~/.tmux.conf
+
+echo "tmux updated"
