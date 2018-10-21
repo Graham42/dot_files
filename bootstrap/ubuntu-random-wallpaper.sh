32a8a29382966a2afbf0b106de76c877a0214e31
Graham McGregor <graham@gmcgregor.ca>
2018-10-20 20:56:01 -0400

    Update background script

--- bootstrap/ubuntu-random-wallpaper.sh
+++ bootstrap/ubuntu-random-wallpaper.sh
@@ -1,16 +1,28 @@
 #!/usr/bin/env bash
 
-set -ex
-set -o pipefail
-
-mkdir -p ~/Pictures/Wallpapers
-
 # Download random background / lockscreen wallpapers
+# For GNOME desktop. Tested on Ubuntu 18.04
 
-RANDOM_NAME=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')
-curl -JL -o "$HOME/Pictures/Wallpapers/${RANDOM_NAME}.jpeg" https://picsum.photos/g/2560/1440?random
-gsettings set org.gnome.desktop.background picture-uri "file://$HOME/Pictures/Wallpapers/${RANDOM_NAME}.jpeg"
+set -eo pipefail
 
-RANDOM_NAME=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '')
-curl -JL -o "$HOME/Pictures/Wallpapers/${RANDOM_NAME}.jpeg" https://picsum.photos/g/2560/1440?random
-gsettings set org.gnome.desktop.screensaver picture-uri "file://$HOME/Pictures/Wallpapers/${RANDOM_NAME}.jpeg"
+WALLPAPER_FOLDER="$HOME/Pictures/Wallpapers/"
+mkdir -p "$WALLPAPER_FOLDER"
+
+# Change these to match your screen resolution
+WIDTH=2560
+HEIGHT=1440
+
+downloadRandomImage() {
+    # pick a random image from the ones available for this resolution
+    IMAGE_ID=$(curl -Ls -w "%{url_effective}" -o /dev/null "https://picsum.photos/$WIDTH/$HEIGHT/?random" | grep -o "[0-9]\+$")
+    # Download the chosen image
+    curl -JL -o "$WALLPAPER_FOLDER/image-$IMAGE_ID.jpeg" "https://picsum.photos/g/$WIDTH/$HEIGHT?image=$IMAGE_ID"
+    # Return the path of the downloaded file
+    echo "$WALLPAPER_FOLDER/image-$DESKTOP_ID.jpeg"
+}
+
+DESKTOP_IMAGE=$(downloadRandomImage)
+gsettings set org.gnome.desktop.background picture-uri "file://$DESKTOP_IMAGE"
+
+LOCKSCREEN_IMAGE=$(downloadRandomImage)
+gsettings set org.gnome.desktop.screensaver picture-uri "file://$LOCKSCREEN_IMAGE"
