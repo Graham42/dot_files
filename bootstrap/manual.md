2edb610c35e51f08512a82195c7c59dc14f2e30b
Graham McGregor <graham@gmcgregor.ca>
2018-10-31 22:43:04 -0400

    notes

--- bootstrap/manual.md
+++ bootstrap/manual.md
@@ -1,5 +1,9 @@
 # Dropbox
 
 - Download and install from https://www.dropbox.com/install-linux
-- Create desktop shortcut in ~/.cofig/autostart folder
+- Create desktop shortcut in ~/.config/autostart folder
 
+# VPN
+
+- Download and install PIA VPN
+- Create desktop shortcut in ~/.config/autostart folder
--- setup_scripts/vscode.sh
+++ setup_scripts/vscode.sh
@@ -19,13 +19,15 @@ ms-python.python
 ms-vsliveshare.vsliveshare
 naco-siren.gradle-language
 PeterJausovec.vscode-docker
-rebornix.Ruby
+rebornix.ruby
 robinbentley.sass-indented
 streetsidesoftware.code-spell-checker
 vscodevim.vim
 EOF
 
+# TODO don't launch if there's nothing to remove
 comm -23 <(code --list-extensions | sort) <(sort "$MY_PLUGINS") | xargs -L1 code --uninstall-extension
 
+# TODO only install what's needed
 xargs -L1 code --install-extension <"$MY_PLUGINS"
 
