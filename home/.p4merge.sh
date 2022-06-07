a70445e56142b104660b8c13f04ec567e6e2baaa
Graham McGregor <graham@gmcgregor.ca>
2022-06-07 15:13:38 -0400

    make p4merge work with WSL as well

---
+++ home/.p4merge.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+BASE=$1
+LOCAL=$2
+REMOTE=$3
+MERGED=$4
+
+set -x
+
+if grep -i -q microsoft /proc/version; then
+  p4merge.exe -le unix "$BASE" "$LOCAL" "$REMOTE" "$MERGED"
+else
+  p4merge -le unix "$BASE" "$LOCAL" "$REMOTE" "$MERGED"
+fi
