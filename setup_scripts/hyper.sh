596ab69d480c30502bcaf4afa4dde46d1800ded3
Graham McGregor <graham@gmcgregor.ca>
2022-06-06 22:40:30 -0400

    don't fail setup on Windows if Hyper isn't installed
    For reference:
    - https://github.com/microsoft/terminal
    - https://hyper.is/

--- setup_scripts/hyper.sh
+++ setup_scripts/hyper.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# If we're running in WSL, we're going to be using Windows Terminal, not Hyper
+if grep -i -q microsoft /proc/version; then
+  exit 0;
+fi
+
 if [ -e "/usr/share/applications/hyper.desktop" ]; then
   mkdir -p "$HOME/.config/autostart/"
   ln -sf /usr/share/applications/hyper.desktop "$HOME/.config/autostart/hyper.desktop"
