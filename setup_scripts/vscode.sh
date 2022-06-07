4f3cb3cbec71983cf8039237f4e6c4130ce7b6c9
Graham McGregor <graham@gmcgregor.ca>
2022-06-06 23:57:54 -0400

    Install Windows side VS Code plugins

--- setup_scripts/vscode.sh
+++ setup_scripts/vscode.sh
@@ -22,7 +22,6 @@ comm -23 \
     <(sort -f "$MY_PLUGINS" | tr "[:upper:]" "[:lower:]") \
     <(code --list-extensions | sort -f | tr "[:upper:]" "[:lower:]")
 )
-
 if [ -z "$MISSING_PLUGINS" ] ; then
     echo "All vscode plugins already installed"
 else
@@ -31,8 +30,42 @@ fi
 
 echo ""
 echo "Extra vscode plugins installed:"
-echo "---"
 comm -23 \
     <(code --list-extensions | sort -f | tr "[:upper:]" "[:lower:]") \
     <(sort -f "$MY_PLUGINS" | tr "[:upper:]" "[:lower:]")
 echo ""
+
+# If we're in WSL we need to repeat the process, but with a subset of
+# extensions. These can be found by opening VSCode after installing from the
+# linux side and looking for plugins that have the option to "Install Locally"
+if grep -i -q microsoft /proc/version; then
+    echo "WSL detected, checking for more plugins..."
+
+    cat >"$MY_PLUGINS" <<EOF
+ms-vscode-remote.remote-wsl
+sdras.night-owl
+silvenon.mdx
+vscodevim.vim
+EOF
+
+    # We have to use the powershell command Set-Location to prevent warnings
+    # being printed about trying to start VSCode from the Windows side with a
+    # linux current working directory
+    MISSING_PLUGINS=$(
+    comm -23 \
+        <(sort -f "$MY_PLUGINS" | tr "[:upper:]" "[:lower:]") \
+        <(powershell.exe "Set-Location -Path \"\$env:USERPROFILE\"; code --list-extensions" | sort -f | tr "[:upper:]" "[:lower:]")
+    )
+    if [ -z "$MISSING_PLUGINS" ] ; then
+        echo "All vscode plugins already installed"
+    else
+        echo "$MISSING_PLUGINS" | xargs -I{} powershell.exe "Set-Location -Path \"\$env:USERPROFILE\"; code --install-extension {}"
+    fi
+
+    echo ""
+    echo "Extra vscode plugins installed:"
+    comm -23 \
+        <(powershell.exe "Set-Location -Path \"\$env:USERPROFILE\"; code --list-extensions" | sort -f | tr "[:upper:]" "[:lower:]") \
+        <(sort -f "$MY_PLUGINS" | tr "[:upper:]" "[:lower:]")
+    echo ""
+fi
