32e084fbec26fc29c231a4647f676832809223bd
Graham McGregor <graham@gmcgregor.ca>
2022-06-07 00:35:57 -0400

    split nvm setup into separate script
    This is easier to read separate from the rest of the system setup

--- bootstrap/ubuntu.sh
+++ bootstrap/ubuntu.sh
@@ -132,26 +132,7 @@ install_if_needed \
     dconf-editor
 
 sudo apt-get upgrade -y
-# Install nvm
-LATEST_NVM_VERSION=$(curl -L --silent "https://api.github.com/repos/nvm-sh/nvm/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
-# this is super noisy with -x
-set +x
-# Try an load nvm if it's already installed
-# shellcheck source=/dev/null
-source "$HOME/.nvm/nvm.sh" > /dev/null 2>&1 || true
-CURRENT_NVM_V=$(nvm --version || echo NOPE)
-if [ "$LATEST_NVM_VERSION" != "v$CURRENT_NVM_V" ]; then
-    curl -L -f -o- "https://raw.githubusercontent.com/nvm-sh/nvm/$LATEST_NVM_VERSION/install.sh" | bash
-
-    export NVM_DIR="$HOME/.nvm"
-    # shellcheck disable=SC1090
-    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-fi
-echo installing nvm lts/*
-nvm install lts/*
-set -x
-nvm alias default lts/*
-npm install -g npm
+./bootstrap/nvm.sh
 
 LATEST_HYPER=$(curl -L --silent "https://api.github.com/repos/zeit/hyper/releases/latest" |
     grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
