8ca0a12baf6bbed971bf4f3e0465245d4f7d7080
Graham McGregor <graham@gmcgregor.ca>
2022-06-28 22:53:19 -0400

    clean up jira git alias

--- home/.git-alias-scripts/jira-commit.sh
+++ home/.git-alias-scripts/jira-commit.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
-# Requires the environment variable JIRA_URL to be set
 
 set -eo pipefail
-
-if [ -z "$JIRA_URL" ]; then
-  echo "ERROR: env var 'JIRA_URL' is not set"
-  exit 1
-fi
 
 if [ -z "$1" ]; then
   id=$(git rev-parse --abbrev-ref HEAD | cut -f2 -d/ | cut -f1 -d_)
