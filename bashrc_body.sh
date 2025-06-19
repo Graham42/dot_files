ffa504e34a3a8e3b085a0a0b49bf83382111a29a
Graham McGregor <graham@gmcgregor.ca>
2025-06-19 15:18:25 -0400

    tweak bashrc

--- bashrc_body.sh
+++ bashrc_body.sh
@@ -1,4 +1,6 @@
 # This block is added to ~/.bashrc automatically by the init script
 if [ -f ~/.bashrc_extend.sh ]; then
     source ~/.bashrc_extend.sh
+else
+    echo "Warning: ~/.bashrc_extend.sh expected but not found. Please ensure it exists."
 fi
